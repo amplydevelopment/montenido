32508df96de7e6ae64ea51ae597b5a0c90f16218
frangoni <fran.goni96@gmail.com>
2025-05-01 14:26:52 -0300

    remove debugging logs SpringHealth

--- SpringHealth.js
+++ SpringHealth.js
@@ -613,7 +613,6 @@ JotForm.isNewSACL = true;
 window.addEventListener('DOMContentLoaded', event => {
 	// Get the current URL
 	var currentUrl = window.location.href;
-	console.log('currentUrl', currentUrl);
 
 	// Check if the URL exceeds 255 characters
 	if (currentUrl.length > 255) {
@@ -625,7 +624,6 @@ window.addEventListener('DOMContentLoaded', event => {
 	const inputField = document.getElementById('input_32');
 	if (inputField) {
 		inputField.value = currentUrl;
-		console.log('inputField', inputField);
 	} else {
 		console.error('Input field with ID "input_32" not found.');
 	}
